4cbafd10da7c22821f24f90454096f05bc3fda78
Doug Hellmann <doug@doughellmann.com>
2014-09-10 12:05:01 -0400

    Remove templates from toctrees
    We don't want the templates to show up in the RSS feed, so exclude them
    from being converted to HTML and listed in the toctree.
    
    Change-Id: I67496730d1952ffdec92c5f78531508c55225ca6

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -70,7 +70,11 @@ copyright = u'2014, OpenStack Barbican Team'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = [
+    '_build',
+    'template.rst',
+    '**/template.rst',
+]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -4,14 +4,6 @@
 Barbican Project Specifications
 ===============================
 
-Contents:
-
-.. toctree::
-   :glob:
-   :maxdepth: 1
-
-   specs/*
-
 Juno approved specs:
 
 .. toctree::
