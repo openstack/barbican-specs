eb5fc837e5ac6e569eac654a268f04668dc6dc8c
Joe Gordon <joe.gordon0@gmail.com>
2015-05-27 15:12:14 -0700

    Drop incubating theme from docs
    OpenStack has dropped the incubation notion, so labeling this as an
    incubated project in the docs is confusing.
    
    Change-Id: I5208ceebba4ce388039a7697fda8b5794db920c2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -111,7 +111,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'incubating': True}
+html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
